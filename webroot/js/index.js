1d8afa1395b52314562c307c25516cb5a9c10515
Mike Flynn <mfflynn@gmail.com>
2016-01-20 19:13:25 -0800

    Added basic version of front page.

---
+++ webroot/js/index.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function() {
+  jQuery("form").submit(function(event) {
+    event.preventDefault();
+    var error = false;
+
+    jQuery('form :input.rqd').each(function() {
+      if(jQuery(this).val() == "") {
+        error = true;
+        jQuery(this).parent().addClass('has-error');
+      }
+    });
+
+    if !error {
+      jQuery('form :input.rqd').each(function() {
+        jQuery(this).prop('disabled', true);
+      });
+
+      jQuery.ajax({
+        url: "/api/user/add/",
+        type: "POST",
+        dataType: "json",
+        data: {
+          name: jQuery('#nameInput').val(),
+          uuid: jQuery('#uuidInput').val(),
+          network: jQuery('#networkInput').val(),
+          state: jQuery('#stateInput').val(),
+          window: jQuery('#windowInput').val()
+        },
+        success: function(data) {
+          alert("Yeah bro!");
+        },
+        error: function() {
+          alert("There was a problem creating your account.");
+        }
+      });
+    }
+
+    return false;
+  });
+});
